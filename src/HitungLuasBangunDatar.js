73e3b8f7e571c86ff1d48f63c389760c80989e59
Oriana Fallacy Br Damanik <orianadamanik17@gmail.com>
2021-06-04 22:17:41 +0700

    tugas1

---
+++ src/HitungLuasBangunDatar.js
@@ -0,0 +1,44 @@
+
+import React,{useState} from "react";
+import './stylebg.css';
+function App(){
+
+  const[alas, setalas] = useState("") 
+  const[tinggi, settinggi] = useState("")
+  const[hitung, sethitung] = useState("")
+
+  console.log(`alas : ${alas}`)
+  console.log(`tinggi : ${tinggi}`)
+
+  function luas(){
+    sethitung(0.5 * alas * tinggi)
+  }
+
+  return(
+    <div class = "App">
+      <h1>
+        MENGHITUNG LUAS SEGITIGA
+      </h1>
+      <p>
+        Masukan Nilai Alas
+      </p>
+      <input type = "text" value = {alas} onChange = {e=> setalas(+e.target.value)}></input>
+
+      <p>
+        Masukan Nilai Tinggi
+      </p>
+      <input type = "text" value = {tinggi} onChange = {e=> settinggi(+e.target.value)}></input>
+
+      <p>
+        <button onClick = {luas}>hitung</button>
+      </p>
+
+
+      <p>
+        {hitung}
+      </p>
+
+    </div>
+  );
+}
+export default App;
